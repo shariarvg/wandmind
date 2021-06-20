2538919fc043b52c3327f9746e7633c93ecb274e
Shariar Vaez <53067471+shariarvg@users.noreply.github.com>
2021-06-19 21:33:39 -0400

    Update settings.py

--- wandering_mind/settings.py
+++ wandering_mind/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'inznhvvvg83jmvlon-f$1@-b)&8@nw%2kqzp$x%(=it)#_l280'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
